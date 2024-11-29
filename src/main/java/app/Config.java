8543d6aee876506ac98b0901d7222716d055f3bb
Mingxiao Wei <mingxiaowei@Mingxiaos-MacBook-Air.local>
2024-11-29 14:51:01 -0500

    Added the functionality of loading api key from api_key.txt to config.

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -1,6 +1,28 @@
 package app;
 
-public class Config {
-    public static final String API_KEY = "<your_api_key>";
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
+public class Config {
+    public static final String API_KEY = initialize_api_key();
+    private static String initialize_api_key() {
+        try (InputStream inputStream = Config.class.getClassLoader().getResourceAsStream("api_key.txt")) {
+            if (inputStream != null) {
+                Scanner sc = new Scanner(inputStream);
+                if (sc.hasNextLine()) {
+                    return sc.nextLine().trim();
+                }
+                else {
+                    throw new RuntimeException("API key not found.");
+                }
+            }
+            else {
+                throw new RuntimeException("Generate input stream unsuccessful.");
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Failed to find api_key.txt");
+        }
+    }
 }
