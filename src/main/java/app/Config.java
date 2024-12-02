816dae4d882f4b293dedeff80d3370ffa77b02c7
chendi12 <dinghui.chen@mail.utoronto.ca>
2024-12-02 04:42:42 -0500

    Ready for test

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -14,17 +14,16 @@ public class Config {
     public static final int DAILY_SAMPLE_SIZE = 60;
     public static final int WEEKLY_SAMPLE_SIZE = 48;
     public static final int INTRADAY_PREDICT_INTERVAL = 5;
+    public static final int INTEREST_RATE = 5;
     public static final int CONCURRENCY_THREAD_COUNT = Runtime.getRuntime().availableProcessors();
     public static final JSONArray STOCK_LIST = initializeStockList();
-    public static final double INTEREST_RATE = 0.05;
-
     private static String initializeAPIKey() {
         try {
             return Files.readString(Paths.get(Objects.requireNonNull(Config
-                    .class
-                    .getClassLoader()
-                    .getResource("/config/api_key.txt"))
-                    .toURI()))
+                                    .class
+                                    .getClassLoader()
+                                    .getResource("config/api_key.txt"))
+                            .toURI()))
                     .trim();
         }
         catch (IOException | URISyntaxException e) {
@@ -34,14 +33,15 @@ public class Config {
     private static JSONArray initializeStockList() {
         try {
             String jsonString = Files.readString(Paths.get(Objects.requireNonNull(Config
-                    .class
-                    .getClassLoader()
-                    .getResource("/config/stock_list.json"))
-                    .toURI()));
+                                    .class
+                                    .getClassLoader()
+                                    .getResource("config/stock_list.json"))
+                            .toURI()))
+                    .trim();
             return new JSONArray(jsonString);
         }
         catch (IOException | URISyntaxException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,55 @@
 package app;
+import data_access.AlphaVantageDataAccessObject;
+import data_access.LocalDataAccessObject;
+import interface_adapters.ViewManagerModel;
+import interface_adapters.buy_stock.BuyStockViewModel;
+import interface_adapters.remove_stock.RemoveStockViewModel;
+import interface_adapters.reset_portfolio.ClearAllViewModel;
+import interface_adapters.view_portfolio.ViewPortfolioController;
+import interface_adapters.view_portfolio.ViewPortfolioPresenter;
+import interface_adapters.view_portfolio.ViewPortfolioViewModel;
+import usecases.LocalDataAccessInterface;
+import usecases.OnlineDataAccessInterface;
+import usecases.view_portfolio.ViewPortfolioInteractor;
+import views.*;
+
+import javax.swing.*;
+import java.awt.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-          AppBuilder appBuilder = new AppBuilder();
+        AppBuilder appBuilder = new AppBuilder();
+        JFrame app = appBuilder
+                .addViewPortfolioView()
+                .addBuyStockView()
+                .addRemoveStockView()
+                .addViewPortfolioUseCase()
+                .addBuyStockUseCase()
+                .addRemoveStockUseCase()
+                .addClearAllUseCase()
+                .build();
+        app.pack();
+        app.setVisible(true);
     }
+
+//    @NotNull
+//    private static JPanel getPanel() {
+//        JPanel buttonPanel = new JPanel();
+//        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+//        JButton predict = new JButton("predict stock price");
+//        JButton managePortfolio = new JButton("manage portfolio");
+//        JButton predictRevenue = new JButton("predict revenue");
+//        JButton backtest = new JButton("backtest");
+//        predict.setAlignmentX(Component.CENTER_ALIGNMENT);
+//        managePortfolio.setAlignmentX(Component.CENTER_ALIGNMENT);
+//        predictRevenue.setAlignmentX(Component.CENTER_ALIGNMENT);
+//        backtest.setAlignmentX(Component.CENTER_ALIGNMENT);
+//        buttonPanel.add(predict);
+//        buttonPanel.add(managePortfolio);
+//        buttonPanel.add(predictRevenue);
+//        buttonPanel.add(backtest);
+//        return buttonPanel;
+//    }
 }
